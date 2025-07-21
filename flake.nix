9b8f71dd8c3eef92b06735a2efaca3982426d57c
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-07-21 09:17:56 -0400

    fix: sops-nix nixpkgs

--- flake.nix
+++ flake.nix
@@ -16,7 +16,10 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
