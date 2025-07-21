183c07c34e1c0798410a2657ca462db098951e7c
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-07-21 00:01:10 -0400

    feat: flake.nix and flakeHelpers heavily borrowed from notthebee

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  nixConfig = {
+    trusted-substituters = [
+      "https://cachix.cachix.org"
+      "https://nixpkgs.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+      "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    sops-nix.url = "github:Mic92/sops-nix";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager-unstable = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+  };
+
+  outputs =
+    { flake-utils, nixpkgs, ... }@inputs:
+    let
+      helpers = import ./flakeHelpers.nix inputs;
+      inherit (helpers) mkMerge mkNixos;
+    in
+    mkMerge [
+      (flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          packages.default = pkgs.mkShell {
+            packages = [
+              pkgs.just
+              pkgs.nixos-rebuild
+            ];
+          };
+        }
+      ))
+      (mkNixos "roque" inputs.nixpkgs [
+        inputs.home-manager.nixosModules.home-manager
+      ])
+    ];
+
+}
---
+++ flakeHelpers.nix
@@ -0,0 +1,43 @@
+# Borrowed heavily from https://github.com/notthebee/nix-config/blob/main/flakeHelpers.nix
+inputs:
+let
+  homeManagerCfg = userPackages: extraImports: {
+    home-manager.userGlobalPkgs = false;
+    home-manager.extraSpecialArgs = {
+      inherit inputs;
+    };
+    home-manager.users.mestruble.imports = [
+      inputs.sops-nix.homeManagerModules.sops
+    ] ++ extraImports;
+    home-manager.backupFileExtension = "bak";
+    home-manager.useUserPackages = userPackages;
+  };
+in
+{
+  mkNixos = machineHostname: nixpkgsVersion: extraModules: rec {
+    deploy.nodes.${machineHostname} = {
+      hostname = machineHostname;
+      profiles.system = {
+        user = "root";
+        sshUser = "mestruble";
+        path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos nixosConfigurations.${machineHostname};
+      };
+    };
+    nixosConfigurations.${machineHostname} = nixpkgsVersion.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {
+        inherit inputs;
+      };
+      modules = [
+        ./machines/nixos/_common
+        ./machines/nixos/${machineHostname}
+        inputs.sops-nix.homeManagerModules.sops
+      ] ++ extraModules;
+    };
+  };
+  mkMerge = inputs.nixpkgs.lib.lists.foldl'
+    (
+      a: b: inputs.nixpkgs.lib.attrsets.recursiveUpdate a b
+    )
+    { };
+}
