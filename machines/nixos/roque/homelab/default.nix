d91712fb2bd8aefaf17cfdd2c29d35892d802356
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-07-30 10:47:58 -0400

    fix: plex user
    Signed-off-by: Matt Struble <4325029+mattstruble@users.noreply.github.com>

--- machines/nixos/roque/homelab/default.nix
+++ machines/nixos/roque/homelab/default.nix
@@ -14,10 +14,4 @@
 
   networking.firewall.allowedTCPPorts = [ 32400 ]; # Allow Plex port
   networking.firewall.allowedUDPPorts = [ 32400 ]; # Allow Plex port
-
-  users.users.plex = {
-    isNormalUser = true;
-    home = "/var/lib/plex";
-    shell = pkgs.bash;
-  };
 }
