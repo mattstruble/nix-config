7e04939a5b18a145756d27de02fedefe9ab92dfe
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2026-02-16 15:49:54 -0500

    fix: comment sdImage

--- machines/nixos/_rpi/sd-image.nix
+++ machines/nixos/_rpi/sd-image.nix
@@ -8,8 +8,8 @@
     # "${pkgs.path}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
   ];
 
-  sdImage = {
-    compressImage = false;
-    imageName = "${config.networking.hostName}.img";
-  };
+  # sdImage = {
+  #   compressImage = false;
+  #   imageName = "${config.networking.hostName}.img";
+  # };
 }
