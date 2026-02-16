6b1fb132567226829327636d2e0db671f4c22142
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2026-02-16 16:43:41 -0500

    fix(rpi): cyclical import for sdimage

--- machines/nixos/_rpi/sd-image.nix
+++ machines/nixos/_rpi/sd-image.nix
@@ -1,15 +1,16 @@
 { config
 , pkgs
 , lib
+, modulesPath
 , ...
 }:
 {
   imports = [
-    # "${pkgs.path}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+    "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
   ];
 
-  # sdImage = {
-  #   compressImage = false;
-  #   imageName = "${config.networking.hostName}.img";
-  # };
+  sdImage = {
+    compressImage = false;
+    imageName = "${config.networking.hostName}.img";
+  };
 }
