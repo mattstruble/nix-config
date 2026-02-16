6c50102916f2538c24afab5b46a062b4b32fc03a
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2026-02-16 14:15:30 -0500

    feat: rpi configs

---
+++ machines/nixos/_rpi/sd-image.nix
@@ -0,0 +1,15 @@
+{ config
+, pkgs
+, lib
+, ...
+}:
+{
+  imports = [
+    "${pkgs.path}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+  ];
+
+  sdImage = {
+    compressImage = false;
+    imageName = "${config.networking.hostName}.img";
+  };
+}
